ca0d22af03c43d57d467abfd3cabe46ac506f317
alivaliyevprouser1 <ali.valiyev.dev@gmail.com>
2024-10-08 03:45:23 +0400

    refactor: refactor hero text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -32,17 +32,10 @@ const Hero = () => {
           </h1>
 
           <p className="anim-hero-content text-lg lg:text-xl xl:text-2xl">
-            <span className="hidden md:inline">
-              Rabalon emerged from a realization that small businesses face
-              significant hurdles in the digital technology space. Our approach
-              is two-pronged: we provide technology consulting services to
-              recommend and implement existing solutions.
-            </span>
-
-            <span className="md:hidden">
-              Rabalon helps small businesses overcome digital tech challenges
-              through consulting and solution implementation.
-            </span>
+            Rabalon emerged from a realization that small businesses face
+            significant hurdles in the digital technology space. Our approach is
+            two-pronged: we provide technology consulting services to recommend
+            and implement existing solutions.
           </p>
         </div>
 
